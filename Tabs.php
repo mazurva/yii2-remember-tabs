18de4bfdbf8aba6ddbe93287eb1f0765130521f6
Vadim Mazur <mazurva@gmail.com>
2017-05-05 16:27:05 +0600

    Update

--- Tabs.php
+++ Tabs.php
@@ -13,21 +13,21 @@ class Tabs extends \yii\bootstrap\Tabs
         parent::init();
         $view = $this->getView();
         /* remember tabs */
-		$script = <<< JS
-		    $(function() {
-		        //save the latest tab (http://stackoverflow.com/a/18845441)
-		        $('a[data-toggle="tab"]').on('click', function (e) {
-		            localStorage.setItem('lastTab', $(e.target).attr('href'));
-		        });
+$script = <<< JS
+    $(function() {
+        //save the latest tab (http://stackoverflow.com/a/18845441)
+        $('a[data-toggle="tab"]').on('click', function (e) {
+            localStorage.setItem('lastTab', $(e.target).attr('href'));
+        });
 
-		        //go to the latest tab, if it exists:
-		        var lastTab = localStorage.getItem('lastTab');
+        //go to the latest tab, if it exists:
+        var lastTab = localStorage.getItem('lastTab');
 
-		        if (lastTab) {
-		            $('a[href="'+lastTab+'"]').click();
-		        }
-		    });
-		JS;
+        if (lastTab) {
+            $('a[href="'+lastTab+'"]').click();
+        }
+    });
+JS;
 		$view->registerJs($script, yii\web\View::POS_END);
     }
 	
