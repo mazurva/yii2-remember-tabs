acc9b0005621d3b8df2ae1a8d6b862346faaff10
Vadim Mazur <mazurva@gmail.com>
2017-05-05 15:48:45 +0600

    Update

---
+++ Tabs.php
@@ -0,0 +1,34 @@
+<?php
+namespace mazurva\bootstrap;
+
+class Tabs extends \yii\bootstrap\Tabs
+{
+	/**
+	@var boolean
+	*/
+	public $remember = false;
+
+	public function init()
+    {
+        parent::init();
+        $view = $this->getView();
+        /* remember tabs */
+		$script = <<< JS
+		    $(function() {
+		        //save the latest tab (http://stackoverflow.com/a/18845441)
+		        $('a[data-toggle="tab"]').on('click', function (e) {
+		            localStorage.setItem('lastTab', $(e.target).attr('href'));
+		        });
+
+		        //go to the latest tab, if it exists:
+		        var lastTab = localStorage.getItem('lastTab');
+
+		        if (lastTab) {
+		            $('a[href="'+lastTab+'"]').click();
+		        }
+		    });
+		JS;
+		$view->registerJs($script, yii\web\View::POS_END);
+    }
+	
+}
